771b246e5af2a270cf47e3d188db45b7f6edeacd
vladdemo1 <v.demchenko@student.csn.khai.edu>
2022-06-03 00:36:09 +0300

    Optimize return json + base crud films + login

--- flask_app/app/controllers/route_controller.py
+++ flask_app/app/controllers/route_controller.py
@@ -2,7 +2,13 @@
 This mod contains main class RouteController about work in views
 """
 
+import flask_login
+from flask_login import logout_user
+from sqlalchemy.exc import IntegrityError
+
 from app.main.database_model import DatabaseModel
+from app.models.user import User
+from app.models.user_login import UserLogin
 
 
 class RouteController:
@@ -18,8 +24,48 @@ class RouteController:
         return DatabaseModel().films_paginate_with_pattern(page, count_film_per_page, search_pattern)
 
     @staticmethod
-    def get_all_films():
+    def get_all_films(page_number: int, count_per_page=10):
         """
         Get all films from database
         """
-        return DatabaseModel().get_all_films()
+        return DatabaseModel().get_all_films(page_number=page_number, count_per_page=count_per_page)
+
+    @staticmethod
+    def get_user_password_hash(user_name):
+        """
+        Get user's password hash
+        """
+        return User.find_by_name(user_name).password
+
+    @staticmethod
+    def get_name_user_login():
+        """
+        Get name current user who is log in
+        """
+        return flask_login.current_user.username
+
+    @staticmethod
+    def get_id_user(username: str) -> int:
+        return DatabaseModel().get_id_user_by_name(username)
+
+    @staticmethod
+    def get_user_by_id(user_id: int) -> User:
+        return DatabaseModel().get_user_by_id(user_id)
+
+    @staticmethod
+    def logout_user():
+        logout_user()
+
+    @staticmethod
+    def add_user_to_database(name: str, email: str, password: str):
+        try:
+            new_user = User(email=email, name=name, password=password)
+            new_user.save_to_db()
+        except IntegrityError:
+            return 400, {'Message': 'User is exists'}
+
+        return 200, new_user.json()
+
+    @staticmethod
+    def get_user_login(name: str, password: str, user_id: int):
+        return UserLogin(username=name, password=password, user_id=user_id)
--- flask_app/app/main/database_model.py
+++ flask_app/app/main/database_model.py
@@ -3,6 +3,7 @@ This mod contains all logic about work with database
 """
 
 from app.models.film import Film
+from app.models.user import User
 
 
 class DatabaseModel:
@@ -11,11 +12,12 @@ class DatabaseModel:
     """
 
     @staticmethod
-    def get_all_films():
+    def get_all_films(page_number: int, count_per_page: int):
         """
         This func can get all films from database return it
         """
-        Film.query.all()
+        films = Film.query.all()
+        return films.paginate(page=page_number, per_page=count_per_page).items
 
     @staticmethod
     def films_paginate_with_pattern(page_number: int, count_per_page: int, pattern: str):
@@ -24,3 +26,12 @@ class DatabaseModel:
         """
         films = Film.query.filter(Film.name.like(pattern + '%'))
         return films.paginate(page=page_number, per_page=count_per_page).items
+
+    @staticmethod
+    def get_id_user_by_name(user_name: str) -> int:
+        user = User.query.filter(User.name == user_name).first()
+        return user.id
+
+    @staticmethod
+    def get_user_by_id(user_id: int):
+        return User.query.filter(User.id == user_id).first()
